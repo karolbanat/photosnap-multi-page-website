f4dda678864b4db2d8b3643998a0fdfddc6b7150
Karol Banat <karol.banat@gmail.com>
2022-12-20 17:11:12 +0100

    Navigation handling

---
+++ src/ts/main.ts
@@ -0,0 +1,15 @@
+const navToggle: HTMLButtonElement = document.querySelector('.toggle-button');
+
+const handleNavigationToggle = (e: Event) => {
+	const button: HTMLButtonElement = e.target as HTMLButtonElement;
+	const wasExpanded: boolean = button.getAttribute('aria-expanded') === 'true';
+	const targetNavID: string = button.getAttribute('aria-controls');
+	const targetNav: HTMLElement = document.querySelector(`#${targetNavID}`);
+	const buttonLabel: string = !wasExpanded ? 'Close menu' : 'Open menu';
+
+	button.setAttribute('aria-expanded', (!wasExpanded).toString());
+	button.setAttribute('aria-label', buttonLabel);
+	targetNav?.toggleAttribute('data-expanded', !wasExpanded);
+};
+
+navToggle.addEventListener('click', handleNavigationToggle);
